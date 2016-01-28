6d5b736fb07375997cd3a96587a7b340da1180d3
Chukin Anton <admin@kusumpharm.com>
2016-01-28 09:34:52 +0200

    Refactored to $resource

--- angular-dpd.js
+++ angular-dpd.js
@@ -2,6 +2,7 @@
 
 var angularDpdSockets = [];
 
+/* global angular */
 angular.module('dpd', []).value('dpdConfig', [])
     .factory('dpdSocket', ['$rootScope', 'dpdConfig', function($rootScope, dpdConfig) {
         if (!dpdConfig.useSocketIo) {
@@ -40,9 +41,8 @@ angular.module('dpd', []).value('dpdConfig', [])
             rawSocket: socket
         };
     }])
-    .factory('dpd', ['$http', '$rootScope', 'dpdConfig', 'dpdSocket', function($http, $rootScope, dpdConfig, dpdSocket) {
+    .factory('dpd', ['$resource', '$rootScope', 'dpdConfig', 'dpdSocket', function($resource, $rootScope, dpdConfig, dpdSocket) {
         var dpd = {};
-        dpd.errors = [];
         dpd.socket = dpdSocket;
         if (angular.isArray(dpdConfig)) {
             dpdConfig = {
@@ -60,137 +60,8 @@ angular.module('dpd', []).value('dpdConfig', [])
             }
         }
 
-        var ef = function(data, status, headers, config) {
-            dpd.errors.push(data);
-        }
-
-        var checkUndefinedFunc = function(f) {
-            if (typeof f == 'undefined')
-                f = function() {};
-
-            return f;
-        }
-
-        var isComplexQuery = function(obj) {
-            if (obj) {
-                for (var k in obj) {
-                    if (obj.hasOwnProperty(k)) {
-                        if (typeof obj[k] === 'object') {
-                            return true;
-                        }
-                    }
-                }
-            }
-            return false;
-        }
-
         dpdConfig.collections.forEach(function(d) {
-            dpd[d] = {};
-            if (!dpdConfig.noCache) {
-                dpd[d].cache = {
-                    all: [],
-                    get: function(id) {
-                        for (var i in dpd[d].cache.all) {
-                            if (dpd[d].cache.all[i].id == id)
-                                return dpd[d].cache.all[i];
-                        }
-                    }
-                };
-            }
-            dpd[d].get = function(o, s, e) {
-                if (typeof o == "string") {
-                    return $http.get(serverRoot + '/' + d + '/' + o, { withCredentials: true }).success(function(data, status, headers, config) {
-                        if (!dpd[d].cache) return;
-                        var add = true;
-                        for (var i in dpd[d].cache.all) {
-                            if (dpd[d].cache.all[i].id == o) {
-                                add = false;
-                                dpd[d].cache.all[i] = data;
-                            }
-                        }
-                        if (add)
-                            dpd[d].cache.all.push(data);
-                    }).success(checkUndefinedFunc(s)).error(ef).error(checkUndefinedFunc(e));
-                } else {
-                    if (typeof o == "function") {
-                        e = s;
-                        s = o;
-                        return $http.get(serverRoot + '/' + d, { withCredentials: true }).success(function(data, status, headers, config) {
-                            if (!dpd[d].cache) return;
-                            dpd[d].cache.all = data;
-                        }).success(checkUndefinedFunc(s)).error(ef).error(checkUndefinedFunc(e));
-                    } else {
-                        if (isComplexQuery(o)) {
-                            var query = encodeURI(JSON.stringify(o));
-                            return $http.get(serverRoot + '/' + d + '?' + query, { withCredentials: true }).success(function(data, status, headers, config) {
-                                if (!dpd[d].cache) return;
-                                dpd[d].cache.all = data;
-                            }).success(checkUndefinedFunc(s)).error(ef).error(checkUndefinedFunc(e));
-                        } else {
-                            return $http.get(serverRoot + '/' + d, {
-                                params: o,
-                                withCredentials: true
-                            }).success(function(data, status, headers, config) {
-                                if (!dpd[d].cache) return;
-                                dpd[d].cache.all = data;
-                            }).success(checkUndefinedFunc(s)).error(ef).error(checkUndefinedFunc(e));
-                        }
-                    }
-                }
-            };
-
-            dpd[d].put = function(id, o, s, e) {
-                return $http.put(serverRoot + '/' + d + '/' + id, o, { withCredentials: true }).success(function(data, status, headers, config) {
-                    if (!dpd[d].cache) return;
-                    var add = true;
-                    for (var i in dpd[d].cache.all) {
-                        if (dpd[d].cache.all[i].id == id) {
-                            add = false;
-                            dpd[d].cache.all[i] = data;
-                        }
-                    }
-                    if (add)
-                        dpd[d].cache.all.push(data);
-                }).success(checkUndefinedFunc(s)).error(ef).error(checkUndefinedFunc(e));
-            };
-
-            dpd[d].post = function(o, s, e) {
-                return $http.post(serverRoot + '/' + d + '/', o, { withCredentials: true }).success(function(data, status, headers, config) {
-                    if (!dpd[d].cache) return;
-                    dpd[d].cache.all.push(data);
-                }).success(checkUndefinedFunc(s)).error(ef).error(checkUndefinedFunc(e));
-            };
-
-            dpd[d].del = function(id, s, e) {
-                return $http.delete(serverRoot + '/' + d + '/' + id, { withCredentials: true }).success(function(data, status, headers, config) {
-                    if (!dpd[d].cache) return;
-                    for (var i in dpd[d].cache.all) {
-                        if (dpd[d].cache.all[i].id == id) {
-                            dpd[d].cache.all.splice(i, 1);
-                        }
-                    }
-                }).success(checkUndefinedFunc(s)).error(ef).error(checkUndefinedFunc(e));
-            };
-
-            dpd[d].save = function(obj, s, e) {
-                if (typeof obj.id == 'string') {
-                    dpd[d].put(obj.id, obj, s, e);
-                } else {
-                    dpd[d].post(obj, s, e);
-                }
-            };
-            
-            dpd[d].exec = function (func, o, s, e) {
-                return $http.post(serverRoot + '/' + d + '/' + func, o, { withCredentials: true }).success(checkUndefinedFunc(s)).error(ef).error(checkUndefinedFunc(e));                
-            }
-
-            dpd[d].on = function(scope, event, f) {
-                if (!dpdSocket) return;
-                dpdSocket.on(d + ':' + event, f);
-                scope.$on("$destroy", function(){
-                    dpdSocket.removeListener(d + ':' + event, f);
-                })
-            };
+            dpd[d] = $resource(serverRoot+'/'+d+'/:id');
         });
 
         dpd.on = function(scope, event, f) {
